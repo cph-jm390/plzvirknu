b658ea2e41faad22f1f5157952ca097060def903
Johan M <cph-jm390@cphbusiness.dk>
2022-09-22 12:10:00 +0200

    git ignore tilføjes

--- src/DB/Facade.java
+++ src/DB/Facade.java
@@ -3,8 +3,13 @@ package DB;
 import Entitet.Bog;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Facade {
+    public static List<Bog> hentBøger() throws SQLException {
+        return BogMapper.hentBøger();
+    }
+
     public static Bog opretBog(Bog bog) throws SQLException {
         return BogMapper.opretBog(bog);
 
--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,13 @@ import Entitet.Bog;
 import MineUtil.BrugerInput;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-//Bog bog = new Bog("lille ole i skoven","ole hansen");
-       // Facade.opretBog(bog);
+        List <Bog> bogList = new ArrayList<>();
+
 
         while(true){
 
@@ -16,9 +18,19 @@ public class Main {
                     Bog bog = new Bog(BrugerInput.getString("Angiv title"),
                             BrugerInput.getString("Angiv forfatter"));
                     System.out.println("du har nu oprettet bogen: " + Facade.opretBog(bog).toString());
+                    break;
+                case "hent bøger":
+                    bogList=Facade.hentBøger();
+                    udskriv(bogList);
             }
         }
 
 
+    }
+
+    public static void udskriv (List <Bog> objectList){
+    for (Bog bog:objectList){
+        System.out.println(bog.toString());
+    }
     }
 }
